861ad873d18560e544f4e8767b6e6b1e04c98514
talkshrey <talkshrey@gmail.com>
2022-03-20 11:20:45 +0530

    views changed

--- fir/views.py
+++ fir/views.py
@@ -26,7 +26,7 @@ class ReportView(APIView):
         return Response(serializer.data)
 
     def post(self, request, format=None):
-        serializer = HelplineList(data=request.data)
+        serializer = ReportList(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
